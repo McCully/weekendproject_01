f2aad525fae9baab1936cad378a37cb888238024
Mat McCully <mccully1894@gmail.com>
2016-08-26 10:57:18 -0500

    inital commit

---
+++ server/public/scripts/app.js
@@ -0,0 +1,52 @@
+var myApp = angular.module('myApp', []);
+
+
+
+// $(document).ready(function() {
+//   var array = [];
+//   $("#employeeinfo").on('submit' , function(event) {
+//       event.preventDefault();
+//
+//       var values = {};
+//       var fields = $('#employeeinfo').serializeArray();
+//
+//       fields.forEach(function(element, field , array) {
+//         values[element.name] = element.value;
+//       });
+//
+//       array.push(values);
+//
+//       $("#employeeinfo").find('input[type=text]').val('');
+//         appendDom(values);
+//   });
+//
+//
+//   var employeeArray
+//   function appendDom(empInfo) {
+//     $("#container").append('<div class="person"></div>');
+//     var $el = $("#container").children().last();
+//     $el.append('<p>' + 'Employee: ' + empInfo.employeefirstname + ' ' + empInfo.employeelastname + ' ' + '<br>' + 'Id: ' + empInfo.employeeidnumber + '<br>' + 'Title: ' + empInfo.employeejobtitle + '<br> ' + 'Salary: ' + empInfo.employeesalary+ '</p>');
+//     var totalSalary = 0;
+//     array.forEach(function(employee, i) {
+//       totalSalary = totalSalary + parseInt(array[i].employeesalary);
+//     });
+//     var monthlyCost = (totalSalary / 12).toFixed(2);
+//     $('.monthlycost').empty();
+//     $('.monthlycost').append('<p class="monthlycost">Monthly Salary Cost: ' + '$ ' + monthlyCost + '</p>');
+//   };
+//
+//   $('button').on('click' , function() {
+//     var $last = $('#container').children().last();
+//     jQuery.data( monthlycost , 'monthlyCost')
+//     $last.remove('.person');
+//     array.pop();
+//     monthlyCost = 0;
+//     for (var i = 0; i < array.length; i++) {
+//        var count =Number(array[i].employeesalary);
+//         monthlyCost += count;
+//        console.log(monthlyCost);
+//      }
+//
+//
+//   });
+//   });
---
+++ server/public/scripts/controllers/empController.js
@@ -0,0 +1,44 @@
+myApp.controller("empController" , ["$scope" , function($scope){
+  console.log("employee controller up!");
+  // $scope.userImput = "";
+  $scope.array = [];
+  // $scope.employeelastname = ""
+  // $scope.employeefirstname = "";
+  // $scope.employeejobtitle = "";
+  // $scope.employeesalary = "";
+  // $scope.employeeidnumber = "";
+  $scope.subEmp = function(){
+    $scope.values = {
+      name: $scope.employeelastname + ' ' + $scope.employeefirstname,
+      id: $scope.employeeidnumber,
+      title: $scope.employeejobtitle,
+      salary: $scope.employeesalary
+    };
+    $scope.array.push($scope.values);
+
+    var total = 0;
+    var monthlyCost = 0;
+    for(var i = 0; i < $scope.array.length; i++) {
+      total += parseInt($scope.array[i].salary);
+      monthlyCost = total / 12;
+    }
+    $scope.message = monthlyCost.toFixed(2);
+
+    console.log($scope.array.length);
+    console.log($scope.array);
+    console.log(total);
+    console.log(monthlyCost);
+
+  };
+}]);
+    // $scope.values.getownPropertyNamesforEach(function($scope.values){
+    //   console.log($scope.values.salary += $scope.values.salary);
+    // })
+    // $scope.total = 0;
+    // for(var i = 0; i <= $scope.array.length; i++){
+    //   $scope.total += ($scope.array[i].salary / 12);
+    //   console.log($scope.total);
+    //   console.log($scope.array[i].salary);
+    //   return $scope.total;
+    // }
+    // $scope.message = monthlyCost
